b377d78547559b9cbba04fb03ea6b0130f22b28e
Joseph-Harburg <josephharburg@gmail.com>
2018-11-17 13:20:09 -0500

    Added human player object and inserted it into game

--- rpsjharburgv1.py
+++ rpsjharburgv1.py
@@ -18,31 +18,42 @@ class Player:
         pass
 
 
-def beats(one, two):
-    return ((one == 'rock' and two == 'scissors') or
-            (one == 'scissors' and two == 'paper') or
-            (one == 'paper' and two == 'rock'))
+    # def beats(one, two):
+    #     return ((one == 'rock' and two == 'scissors') or
+    #             (one == 'scissors' and two == 'paper') or
+    #             (one == 'paper' and two == 'rock'))
 
 class RandomPlayer(Player):
     def move(self):
         randommove = random.choice(moves)
         return randommove
 
-
+class HumanPlayer(Player):
+    def move(self):
+        humanmove = input("What is your move?:")
+        invalidmove = "Im sorry thats not a valid move please try again."
+        if humanmove not in moves:
+            return invalidmove
+        else:
+            return humanmove
 
 class Game:
     def __init__(self, p1, p2):
-        self.p1 = RandomPlayer()
+        self.p1 = HumanPlayer()
         self.p2 = RandomPlayer()
         self.scorep1 = 0
         self.scorep2 = 0
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
-        print(f"Player 1: {move1}  Player 2: {move2}")
 
-
+        if move1 == "Im sorry thats not a valid move please try again.":
+            print (move1)
+        while move1 == "Im sorry thats not a valid move please try again.":
+            move1 = self.p1.move()
+            print("Im sorry thats not a valid move please try again.")
         if move1 == 'rock':
+            print(f"Player 1: {move1}  Player 2: {move2}")
             if move1 == move2: #if the moves are the same
                 self.scorep1 += 1
                 self.scorep2 += 1
@@ -57,6 +68,7 @@ class Game:
                 return print(f'Player 2 wins! Player 1 has {self.scorep1} points. Player 2 has {self.scorep2} points.')
 
         if move1 == 'paper':
+            print(f"Player 1: {move1}  Player 2: {move2}")
             if move1 == move2: #if the moves are the same
                 self.scorep1 += 1
                 self.scorep2 += 1
@@ -71,6 +83,7 @@ class Game:
                 return  print(f'Player 2 wins! Player 1 has {self.scorep1} points. Player 2 has {self.scorep2} points.')
 
         if move1 == 'scissors':
+            print(f"Player 1: {move1}  Player 2: {move2}")
             if move1 == move2: #if the moves are the same
                 self.scorep1 += 1
                 self.scorep2 += 1
